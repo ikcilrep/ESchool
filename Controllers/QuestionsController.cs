2860eb24a36cb65121bcef5b3bbeb06eb584a873
ikcilrep <frajerzycki@protonmail.com>
2020-04-04 22:24:15 +0200

    Add Question editing

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using ESchool.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ESchool.Models
 {
@@ -32,5 +33,23 @@ namespace ESchool.Models
             return Redirect("/Quiz/Details/" + id);
         }
 
+        [Authorize]
+        public IActionResult Edit(int id)
+        {
+            return View(_context.Questions.First(q => q.Id == id));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Edit(int id, Question question)
+        {
+            var questionToEdit = _context.Questions.First(q => q.Id == id);
+            question.Id = questionToEdit.Id;
+            question.Quiz = questionToEdit.Quiz;
+            _context.Entry(questionToEdit).CurrentValues.SetValues(question);
+            _context.SaveChanges();
+            var quizId = _context.Quizzes.Include(q => q.Questions).First(q => q.Questions.Any(qu => qu.Id == id)).Id;
+            return Redirect("/Quiz/Details/" + quizId);
+        }
     }
 }
